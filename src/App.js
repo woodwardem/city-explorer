2603ec0a6ea8f4a61a4659c12f2d3336d0c9428f
woodwardem <woodwardem@g.crookcountyschools.org>
2022-01-19 08:52:50 -0800

    doing step 2 in lab 6

--- src/App.js
+++ src/App.js
@@ -1,49 +1,67 @@
 
 import React from 'react';
 import './App.css';
+import Axios from 'axios';
+import axios from 'axios';
+import { Container } from 'react-bootstrap/Container';
 class App extends React.Component {
 
-constructor(props) {
-  super(props);
-  this.state = {
-    city: ''
-  };
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: ''
+    locationObj: {}
+    };
+  }
 
 
 
-handleChange = (event)  => {
-  let typedCity = event.target.value;
-  this.setState( {city:typedCity })
-  //console.log(typedCity);
-} 
+  handleChange = (event) => {
+    let typedCity = event.target.value;
+    this.setState({ city: typedCity })
+    console.log(typedCity);
+  }
+  getLocation = async (event) => {
+    event.preventDefault();
+    const url = 'https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json';
+    console.log('URL:', url)
+    let response = await axios.get(url);
+    console.log('Response:', response.data[0]);
+    this.setState({
+      locationObj: response.data[0]
+    })
+  }
 
-render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>City Explorer</h1>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>City Explorer</h1>
+
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-      <form>
-      
-       Your City: <input type="text" name="yourcity" onChange ={this.handleChange} />
-        <button>Search</button>
-      </form>
-      <h2>here is the map for {this.state.city} </h2>
+        </header>
+        <form onSubmit={this.getLocation}>
+
+          Your City: <input type="text" name="yourcity" onChange={this.handleChange} />
+          <button type='submit'>Search</button>
+        </form>
+        {this.state.locationObj.display_name &&
+          <Container>
+            <h2>here is the map for {this.state.locationObj.display_name} </h2>
+            <p>Lat/Lon: {this.state.locationObj.lat}, {this.state.locationObj.lon}</p>
     </div>
   );
+      }
   }
 };
 export default App;
