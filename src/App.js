bb1b2363e2f7215d603ea68d24c1122a7ea51fa6
woodwardem <woodwardem@g.crookcountyschools.org>
2022-01-19 21:20:09 -0800

    almost completed step 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import './App.css';
-import Axios from 'axios';
 import axios from 'axios';
 import { Container } from 'react-bootstrap/Container';
 class App extends React.Component {
@@ -9,7 +8,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: ''
+      city: '',
     locationObj: {}
     };
   }
@@ -18,9 +17,9 @@ class App extends React.Component {
 
   handleChange = (event) => {
     let typedCity = event.target.value;
-    this.setState({ city: typedCity })
+    this.setState({ city: typedCity });
     console.log(typedCity);
-  }
+  };
   getLocation = async (event) => {
     event.preventDefault();
     const url = 'https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json';
@@ -29,7 +28,7 @@ class App extends React.Component {
     console.log('Response:', response.data[0]);
     this.setState({
       locationObj: response.data[0]
-    })
+    });
   }
 
 
@@ -39,29 +38,22 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <h1>City Explorer</h1>
-
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
         </header>
         <form onSubmit={this.getLocation}>
 
-          Your City: <input type="text" name="yourcity" onChange={this.handleChange} />
+          Your City: {" "}
+          <input type="text" name="yourcity" onChange={this.handleChange} />
           <button type='submit'>Search</button>
         </form>
         {this.state.locationObj.display_name &&
           <Container>
             <h2>here is the map for {this.state.locationObj.display_name} </h2>
             <p>Lat/Lon: {this.state.locationObj.lat}, {this.state.locationObj.lon}</p>
+            </Container>
+  }
     </div>
   );
       }
   }
-};
+
 export default App;
